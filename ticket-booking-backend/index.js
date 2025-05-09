82a90f8a16790e391622ea74729a94ca18f733ad
Nikhitha1809 <nikhithac@italentdigital.com>
2025-05-09 14:14:09 +0530

    added frontend url to cors

--- ticket-booking-backend/index.js
+++ ticket-booking-backend/index.js
@@ -15,7 +15,9 @@ app.use(express.json());
 app.use(
     cors({
         origin:[
-            "http://localhost:5173","*"
+            "http://localhost:5173",
+            "https://movie-ticketbooking.vercel.app/",
+            "*"
         ],
         methods:[ "GET","POST","PUT","PATCH","DELETE"],
         allowedHeaders:[
